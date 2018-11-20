f9f3a5d528f6596ec201c551bf78ded2af969f8f
muhib27 <muhib.raven@gmail.com>
2018-11-20 23:04:48 +0600

    as

--- app/src/main/java/com/classtune/ndc/activity/LoginActivity.java
+++ app/src/main/java/com/classtune/ndc/activity/LoginActivity.java
@@ -228,6 +228,9 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 //                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
 //                        startActivity(intent);
 //                        finish();
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
                         Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
                         uiHelper.dismissLoadingDialog();
                     }
